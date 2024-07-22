e2e0c6cdee3fd20f85b02fd967bd02e933a678d3
Hernan G. Gonzalez <camus.sonico@gmail.com>
2024-07-21 23:38:01 -0300

    Stage 4

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use std::{
+    fmt::Display,
     io::{self, Write},
+    process::exit,
     str::FromStr,
 };
 
 #[derive(Debug)]
-enum Command {}
+enum Command {
+    Exit(i32),
+}
 
 impl FromStr for Command {
     type Err = anyhow::Error;
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
-        let msg = format!("{}: command not found", s);
-        anyhow::bail!(msg)
+        let mut parts = s.split_whitespace();
+        let cmd = parts.next().context("missing command")?;
+        match cmd {
+            "exit" => Ok(Self::Exit(parts.next().unwrap_or("0").parse()?)),
+            e => anyhow::bail!("{e}: command not found"),
+        }
+    }
+}
+
+impl Display for Command {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Exit(value) => write!(f, "exit {value}"),
+        }
+    }
+}
+
+fn handle_command(c: Command) {
+    match c {
+        Command::Exit(code) => exit(code),
     }
 }
 
@@ -30,7 +52,7 @@ fn main() -> Result<()> {
         for line in input.lines() {
             let res: Result<Command> = line.parse();
             match res {
-                Ok(_) => todo!(),
+                Ok(cmd) => handle_command(cmd),
                 Err(e) => println!("{e}"),
             }
         }
