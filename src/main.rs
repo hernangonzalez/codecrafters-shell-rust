642e09830c12b533b3c97b669232860f525eaeb2
Hernan G. Gonzalez <camus.sonico@gmail.com>
2024-07-21 21:47:15 -0300

    Stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use anyhow::Result;
-use std::io::{self, Write};
+use std::{
+    io::{self, Write},
+    str::FromStr,
+};
+
+#[derive(Debug)]
+enum Command {}
+
+impl FromStr for Command {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let msg = format!("{}: command not found", s);
+        anyhow::bail!(msg)
+    }
+}
 
 fn main() -> Result<()> {
     print!("$ ");
@@ -9,6 +23,15 @@ fn main() -> Result<()> {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input)?;
+
+    // Parse input
+    for line in input.lines() {
+        let res: Result<Command> = line.parse();
+        match res {
+            Ok(_) => todo!(),
+            Err(e) => println!("{e}"),
+        }
+    }
 
     Ok(())
 }
